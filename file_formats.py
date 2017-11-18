ed18f63056ed442bef8fd3d93b06004dab16f579
linchong <383105690@qq.com>
2017-11-18 20:30:50 +0800

    add file_formats.py def excel_demo

--- file_formats.py
+++ file_formats.py
@@ -1,5 +1,7 @@
 import csv
 from xml.etree.ElementTree import Element, ElementTree
+import xlrd, xlwt
+import xlrd.sheet
 
 def csv_demo():
     with open('test.csv', 'r') as f:
@@ -28,6 +30,25 @@ def xml_demo():
     et = ElementTree(root)
     et.write('test.xml')
 
+def excel_demo():
+    # 向原表中添加总分字段
+    rbook = xlrd.open_workbook('demo.xlsx')
+    rsheet = rbook.sheet_by_index(0)
+    new_col = rsheet.ncols
+    rsheet.put_cell(0, new_col, xlrd.XL_CELL_TEXT, '总分', None)
+    for row in range(1, rsheet.nrows):
+        total_score = sum(rsheet.row_values(row, 1))
+        rsheet.put_cell(row, new_col, xlrd.XL_CELL_NUMBER, total_score, None)
+
+    # 将原表中的数据写到新表中并保存
+    wbook = xlwt.Workbook()
+    wsheet = wbook.add_sheet(rsheet.name)
+    style = xlwt.easyxf('align: vertical center, horizontal center')
+    for row in range(rsheet.nrows):
+        for col in range(rsheet.ncols):
+            wsheet.write(row, col, rsheet.cell_value(row, col), style)
+
+    wbook.save('new_demo.xls')  # xlwt模块生成的文件后缀为xls，不能用xlsx
 
 if __name__ == '__main__':
-    xml_demo()
+    excel_demo()
